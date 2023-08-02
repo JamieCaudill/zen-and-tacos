5cfa31c75131a1b86d8f849e1f8e866c80dfce5f
Jamie Caudill <j.caudill7177@gmail.com>
2023-08-02 09:35:46 -0700

    feat: create Error component

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,1 +1,14 @@
 // Error.js //
+
+import './Error.css';
+
+const Error = () => {
+  return (
+    <section className="error">
+      <h2 className="error__message">Sorry, something went wrong. Please try again later.</h2>
+    </section>
+  )
+}
+
+export default Error;
+
