63b1afc3eb94a2f982e951b896f470581c2e64e4
Jamie Caudill <j.caudill7177@gmail.com>
2023-08-03 08:32:42 -0700

    feat: Add loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,13 @@
+// Loading.js //
+
+import './Loading.css';
+
+const Loading = () => {
+  return (
+    <section className="loading">
+      <h2 className="loading__text">Loading...</h2>
+    </section>
+  )
+}
+
+export default Loading;
